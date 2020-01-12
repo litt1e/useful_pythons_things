f0d99a5402d9189ddca8a490f1b6daafc387f58e
Sashka <osninogi@gmail.com>
2020-01-12 22:04:06 +0300

    update 2

--- bot.py
+++ bot.py
@@ -9,18 +9,6 @@ bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
 
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    print("Started new user")
-    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
-
-
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    print("Message has been got")
-    bot.reply_to(message, message.text)
-
-
 @server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
@@ -31,6 +19,18 @@ def webhook():
     bot.remove_webhook()
     bot.set_webhook(url='https://shlyapikbot.heroku.com/' + TOKEN)
     return "!", 200
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    print("Started new user")
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    print("Message has been got")
+    bot.reply_to(message, message.text)
 
 
 if __name__ == "__main__":
